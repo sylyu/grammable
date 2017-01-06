1cf386d52a191af83674435e0a0117aa7d332139
Sylyu <sylyu>
2017-01-06 20:26:34 +0000

    add validation on grams - save point

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'rails_12factor', group: :production
 gem 'bootstrap-sass', '~> 3.3.5.1'
 gem 'sprockets', '2.11.0'
 gem 'simple_form'
+gem 'devise', '>= 3.2.4'
+
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -15,13 +15,19 @@ RSpec.describe GramsController, type: :controller do
     end
   end
 
-  describe "rams#create action" do
+  describe "grams#create action" do
     it "should successfully create a new gram in our database" do
       post :create, gram: {message: 'Hello!'}
       expect(response).to redirect_to root_path
 
       gram = Gram.last
       expect(gram.message).to eq("Hello!")
+
+      it "should properly deal with validation errors" do
+        post :create, gram: {message: ''}
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(Gram.count).to eq 0
+      end
     end
   end
 
